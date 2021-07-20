c1993109206e344e745d79a4e9d750e56ebb0c98
Abdulrahman <boodynaser@rocketmail.com>
2021-07-20 22:21:51 +0200

    Add capitalize function and run test successfully

--- stringCapitalize.test.js
+++ stringCapitalize.test.js
@@ -3,11 +3,11 @@ const stringcapitalize = require('./stringCapitalize');
 describe ('Capitalize the first letter of a given string',() =>{
     test('test1',() =>{
         expect(stringcapitalize("abdulrahman")).toBe("Abdulrahman");
-    })
+    });
     test('test2',() =>{
         expect(stringcapitalize("hello my name is abdulrahman")).toBe("Hello my name is abdulrahman");
-    })
+    });
     test('test3',() =>{
         expect(stringcapitalize("alles gut zum opferfest")).toBe("Alles gut zum opferfest");
-    })
+    });
 });
